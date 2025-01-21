bca83d6546f6d28d20560e6c63258d702b8ba8e8
apple <apple@appurunoMacBook-Air.local>
2025-01-21 12:46:47 +0900

    first commit

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<footer>
+        <div class="footer-left">
+            &copy; 2025 My Website
+        </div>
+        <div class="footer-right">
+            Powered by PHP
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include('./components/header.php'); ?>
+<main>
+    <h1>Welcome to Home Page</h1>
+    <p>This is the content of the home page.</p>
+</main>
+<?php include('./components/footer.php'); ?>
