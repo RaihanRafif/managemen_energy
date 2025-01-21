05cb416fa36c9a5d648ddacb116400a17f256a67
apple <apple@appurunoMacBook-Air.local>
2025-01-21 23:03:35 +0900

    add assets

--- screens/home/home.php
+++ screens/home/home.php
@@ -1,11 +1,29 @@
 <div class="container">
     <div class="left">
-        <div class="top"></div>
+        <div class="top">
+            <div class="top-top">
+                <div class="top-top-container">
+                    <div class="img-cont">
+                        
+                    </div>
+                    <div class="data-cont"></div>
+                </div>
+                <div class="top-top-container"></div>
+                <div class="top-top-container"></div>
+            </div>
+            <div class="top-bottom">
+                <div class="top-bottom-left"></div>
+                <div class="top-bottom-right"></div>
+            </div>
+        </div>
         <div class="bottom"></div>
     </div>
     <div class="right">
         <h2>Analysis</h3>
             <h3>Today Production (kWh)</h3>
+            <div class="input">
+                <p>10.0000</p>
+            </div>
             <h3>Today Saving (Rp.)</h3>
             <div class="input">
                 <p>10.0000</p>
